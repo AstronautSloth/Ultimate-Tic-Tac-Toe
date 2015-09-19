97d5c04da73dbd6da600c6e90e5601eb097da727
Colin Thompson <cjt1871@g.rit.edu>
2015-09-19 12:25:20 -0400

    Initial Commit

---
+++ Quantum Tic-Tac-Toe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quantum Tic-Tac-Toe
+//
+//  Created by Colin Thompson on 9/19/15.
+//  Copyright © 2015 Colin Thompson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Quantum Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quantum Tic-Tac-Toe
+//
+//  Created by Colin Thompson on 9/19/15.
+//  Copyright © 2015 Colin Thompson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
