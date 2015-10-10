545de50299646f865106e6459bd1dc1a7fcc27f7
Colin Thompson <cjt1871@g.rit.edu>
2015-10-10 14:58:30 -0400

    Everything seems to be in order, proper tie checking is functional, alerts user to wins and ties. Could make it look nicer if I felt like it.

--- Quantum Tic-Tac-Toe/ViewController.swift
+++ Quantum Tic-Tac-Toe/ViewController.swift
@@ -20,9 +20,18 @@ class ViewController: NSViewController {
     @IBOutlet weak var matrix7: buttonMatrix!
     @IBOutlet weak var matrix8: buttonMatrix!
     
-    var turnNum = 0;
+    @IBOutlet weak var box0: NSBox!
+    @IBOutlet weak var box1: NSBox!
+    @IBOutlet weak var box2: NSBox!
+    @IBOutlet weak var box3: NSBox!
+    @IBOutlet weak var box4: NSBox!
+    @IBOutlet weak var box5: NSBox!
+    @IBOutlet weak var box6: NSBox!
+    @IBOutlet weak var box7: NSBox!
+    @IBOutlet weak var box8: NSBox!
     
-    
+    var turnNum = 0
+    var winner = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,52 +43,126 @@ class ViewController: NSViewController {
             
         }
     }
+    func matrixArr() -> [buttonMatrix!] {
+        let arr = [matrix0,matrix1,matrix2,matrix3,matrix4,matrix5,matrix6,matrix7,matrix8]
+        return arr
+    }
+    
+    func boxArr() -> [NSBox!] {
+        let arr = [box0,box1,box2,box3,box4,box5,box6,box7,box8]
+        return arr
+    }
 
     @IBAction func buttonClicked(sender: buttonMatrix) {
-        let selcell: NSButtonCell = sender.selectedCell() as! NSButtonCell;
+        let selcell: NSButtonCell = sender.selectedCell() as! NSButtonCell
         if(turnNum % 2 == 0){
-            selcell.title = "X";
-            selcell.enabled = false;
+            selcell.title = "X"
+            selcell.enabled = false
         }else{
-            selcell.title = "O";
-            selcell.enabled = false;
+            selcell.title = "O"
+            selcell.enabled = false
         }
         turnNum++;
         
         let index = selcell.tag;
-        if(sender.winner == ""){
-            sender.calculatewinner();
+        
+        sender.calculatewinner()
+        
+        let boxes = self.boxArr()
+        if(sender.winner == "X"){
+            boxes[sender.tag].fillColor = NSColor(red: 0, green: 1, blue: 0, alpha: 0.25)
+            boxes[sender.tag].transparent = false
+        }else if(sender.winner == "O"){
+            boxes[sender.tag].fillColor = NSColor(red: 1, green: 0, blue: 0, alpha: 0.25)
+            boxes[sender.tag].transparent = false
         }
         
-        for(var x = 0; x < 9; x++){
-            if(){
-                
+        let arr = self.matrixArr()
+        for matrix in arr{
+            if(matrix.tag == index){
+                matrix.enableButtons()
+            }else{
+                matrix.disableButtons()
             }
+        }
+        if(arr[index].allButtonsDisabled()){
+            for matrix in arr{
+                matrix.enableButtons()
+            }
+        }
+        
+        self.isThereAWinner()
+        if(self.isTie()){
+            let tieAlert = NSAlert()
+            tieAlert.messageText = "It's a Draw!"
+            tieAlert.informativeText = "Would you like to play again?"
+            tieAlert.alertStyle = NSAlertStyle.WarningAlertStyle
+            tieAlert.addButtonWithTitle("Restart")
+            tieAlert.addButtonWithTitle("Quit")
+            let res = tieAlert.runModal()
+            if(res == NSAlertFirstButtonReturn){
+                self.restart()
+            }else{
+                exit(0)
+            }
+        }
+        
+        if(winner != ""){
+            let winAlert = NSAlert()
+            winAlert.messageText = "The winner is " + winner + "!"
+            winAlert.informativeText = "Would you like to play again?"
+            winAlert.alertStyle = NSAlertStyle.WarningAlertStyle
+            winAlert.addButtonWithTitle("Restart")
+            winAlert.addButtonWithTitle("Quit")
+            let result = winAlert.runModal()
+            if(result == NSAlertFirstButtonReturn){
+                self.restart()
+            }else{
+                exit(0)
+            }
+        }
+        
+    }
+    
+    func isThereAWinner(){
+        if(matrix0.winner == matrix1.winner && matrix0.winner == matrix2.winner && matrix0.winner != ""){
+            winner = matrix0.winner;
+        }else if(matrix3.winner == matrix4.winner && matrix3.winner == matrix5.winner && matrix3.winner != ""){
+            winner = matrix3.winner;
+        }else if(matrix6.winner == matrix7.winner && matrix6.winner == matrix8.winner && matrix6.winner != ""){
+            winner = matrix6.winner;
+        }else if(matrix0.winner == matrix3.winner && matrix0.winner == matrix6.winner && matrix0.winner != ""){
+            winner = matrix0.winner;
+        }else if(matrix1.winner == matrix4.winner && matrix1.winner == matrix7.winner && matrix1.winner != ""){
+            winner = matrix1.winner;
+        }else if(matrix2.winner == matrix5.winner && matrix2.winner == matrix8.winner && matrix2.winner != ""){
+            winner = matrix2.winner;
+        }else if(matrix0.winner == matrix4.winner && matrix0.winner == matrix8.winner && matrix0.winner != ""){
+            winner = matrix0.winner;
+        }else if(matrix2.winner == matrix4.winner && matrix2.winner == matrix6.winner && matrix2.winner != ""){
+            winner = matrix2.winner;
         }
     }
     
-    func isThereAWinner() -> Bool{
-        
-        if(matrix0.winner == matrix1.winner && matrix0.winner == matrix2.winner && matrix0.winner != ""){
+    func isTie() -> Bool {
+        if(matrix0.winner != "" && matrix1.winner != "" && matrix2.winner != "" && matrix3.winner != "" && matrix4.winner != "" && matrix5.winner != "" && matrix6.winner != "" && matrix7.winner != "" && matrix8.winner != "" && self.winner == ""){
             return true;
-        }else if(matrix3.winner == matrix4.winner && matrix3.winner == matrix5.winner && matrix3.winner != ""){
-            return true;
-        }else if(matrix6.winner == matrix7.winner && matrix6.winner == matrix8.winner && matrix6.winner != ""){
-            return true;
-        }else if(matrix0.winner == matrix3.winner && matrix0.winner == matrix6.winner && matrix0.winner != ""){
-            return true;
-        }else if(matrix1.winner == matrix4.winner && matrix1.winner == matrix7.winner && matrix1.winner != ""){
-            return true;
-        }else if(matrix2.winner == matrix5.winner && matrix2.winner == matrix8.winner && matrix2.winner != ""){
-            return true;
-        }else if(matrix0.winner == matrix4.winner && matrix0.winner == matrix8.winner && matrix0.winner != ""){
-            return true;
-        }else if(matrix2.winner == matrix4.winner && matrix2.winner == matrix6.winner && matrix2.winner != ""){
-            return true;
+        }else{
+            return false
         }
-        return false;
+    }
+    
+    func restart() {
+        let matrices = matrixArr()
+        let boxes = boxArr()
+        self.turnNum = 0
+        self.winner = ""
+        
+        for(var i = 0; i < 9; i++){
+            boxes[i].transparent = true
+            matrices[i].reset()
+        }
     }
     
 
 }
-
--- Quantum Tic-Tac-Toe/buttonMatrix.swift
+++ Quantum Tic-Tac-Toe/buttonMatrix.swift
@@ -17,30 +17,36 @@ class buttonMatrix: NSMatrix {
     }
     
     func calculatewinner(){
-        if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(0, column: 1)?.title && self.cellAtRow(0, column: 0)?.title == self.cellAtRow(0, column: 2)?.title){
-            winner = (self.cellAtRow(0, column: 0)?.title)!;
-        }else if(self.cellAtRow(1, column: 0)!.title == self.cellAtRow(1, column: 1)?.title && self.cellAtRow(1, column: 0)?.title == self.cellAtRow(1, column: 2)?.title){
-            winner = (self.cellAtRow(1, column: 0)?.title)!;
-        }else if(self.cellAtRow(2, column: 0)!.title == self.cellAtRow(2, column: 1)?.title && self.cellAtRow(2, column: 0)?.title == self.cellAtRow(2, column: 2)?.title){
-            winner = (self.cellAtRow(2, column: 0)?.title)!;
-        }else if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(0, column: 1)?.title && self.cellAtRow(0, column: 0)?.title == self.cellAtRow(0, column: 2)?.title){
-            winner = (self.cellAtRow(0, column: 0)?.title)!;
-        }else if(self.cellAtRow(1, column: 0)!.title == self.cellAtRow(1, column: 1)?.title && self.cellAtRow(1, column: 0)?.title == self.cellAtRow(1, column: 2)?.title){
-            winner = (self.cellAtRow(1, column: 0)?.title)!;
-        }else if(self.cellAtRow(2, column: 0)!.title == self.cellAtRow(2, column: 1)?.title && self.cellAtRow(2, column: 0)?.title == self.cellAtRow(2, column: 2)?.title){
-            winner = (self.cellAtRow(2, column: 0)?.title)!;
-        }else if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(1, column: 1)?.title && self.cellAtRow(0, column: 0)?.title == self.cellAtRow(2, column: 2)?.title){
-            winner = (self.cellAtRow(0, column: 0)?.title)!;
-        }else if(self.cellAtRow(0, column: 2)!.title == self.cellAtRow(1, column: 1)?.title && self.cellAtRow(0, column: 2)?.title == self.cellAtRow(2, column: 0)?.title){
-            winner = (self.cellAtRow(0, column: 2)?.title)!;
+        if(winner == ""){
+            if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(0, column: 1)!.title &&
+                self.cellAtRow(0, column: 0)!.title == self.cellAtRow(0, column: 2)!.title && self.cellAtRow(0, column: 0)!.title != ""){
+                    winner = (self.cellAtRow(0, column: 0)!.title);
+            }else if(self.cellAtRow(1, column: 0)!.title == self.cellAtRow(1, column: 1)!.title && self.cellAtRow(1, column: 0)!.title == self.cellAtRow(1, column: 2)!.title && self.cellAtRow(1, column: 0)!.title != ""){
+                winner = (self.cellAtRow(1, column: 0)!.title);
+            }else if(self.cellAtRow(2, column: 0)!.title == self.cellAtRow(2, column: 1)!.title && self.cellAtRow(2, column: 0)!.title == self.cellAtRow(2, column: 2)!.title && self.cellAtRow(2, column: 0)!.title != ""){
+                winner = (self.cellAtRow(2, column: 0)!.title);
+            }else if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(1, column: 0)!.title && self.cellAtRow(0, column: 0)!.title == self.cellAtRow(2, column: 0)!.title && self.cellAtRow(0, column: 0)!.title != ""){
+                winner = (self.cellAtRow(0, column: 0)!.title);
+            }else if(self.cellAtRow(0, column: 1)!.title == self.cellAtRow(1, column: 1)!.title && self.cellAtRow(0, column: 1)!.title == self.cellAtRow(2, column: 1)!.title && self.cellAtRow(0, column: 1)!.title != ""){
+                winner = (self.cellAtRow(0, column: 1)!.title);
+            }else if(self.cellAtRow(0, column: 2)!.title == self.cellAtRow(1, column: 2)!.title && self.cellAtRow(0, column: 2)!.title == self.cellAtRow(2, column: 2)!.title && self.cellAtRow(0, column: 2)!.title != ""){
+                winner = (self.cellAtRow(0, column: 2)!.title);
+            }else if(self.cellAtRow(0, column: 0)!.title == self.cellAtRow(1, column: 1)!.title && self.cellAtRow(0, column: 0)!.title == self.cellAtRow(2, column: 2)!.title && self.cellAtRow(0, column: 0)!.title != ""){
+                winner = (self.cellAtRow(0, column: 0)!.title);
+            }else if(self.cellAtRow(0, column: 2)!.title == self.cellAtRow(1, column: 1)!.title && self.cellAtRow(0, column: 2)!.title == self.cellAtRow(2, column: 0)!.title && self.cellAtRow(0, column: 2)!.title != ""){
+                winner = (self.cellAtRow(0, column: 2)!.title);
+            }
+        }
+        if(isTie()){
+            winner = "T"
         }
     }
     
     func enableButtons(){
         for(var row = 0; row < 3; row++){
             for(var col = 0; col < 3; col++){
-                if(self.cellAtRow(row, column: col)?.title == ""){
-                    self.cellAtRow(row, column: col)?.enabled = true;
+                if(self.cellAtRow(row, column: col)!.title == ""){
+                    self.cellAtRow(row, column: col)!.enabled = true;
                 }
             }
         }
@@ -49,11 +55,46 @@ class buttonMatrix: NSMatrix {
     func disableButtons(){
         for(var row = 0; row < 3; row++){
             for(var col = 0; col < 3; col++){
-                if(self.cellAtRow(row, column: col)?.title == ""){
-                    self.cellAtRow(row, column: col)?.enabled = false;
+                if(self.cellAtRow(row, column: col)!.title == ""){
+                    self.cellAtRow(row, column: col)!.enabled = false;
                 }
             }
         }
+    }
+    
+    func allButtonsDisabled() -> Bool{
+        for(var row = 0; row < 3; row++){
+            for(var col = 0; col < 3; col++){
+                if(self.cellAtRow(row, column: col)!.enabled == true){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func isTie() -> Bool {
+        if(winner == ""){
+            for(var x = 0; x < 3; x++){
+                for(var y = 0; y < 3; y++){
+                    if(self.cellAtRow(x, column: y)!.title == ""){
+                        return false
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func reset() {
+        for(var x = 0; x < 3; x++){
+            for(var y = 0; y < 3; y++){
+                self.cellAtRow(x, column: y)?.title = ""
+                self.cellAtRow(x, column: y)?.enabled = true
+            }
+        }
+        self.winner = ""
     }
     
 }
